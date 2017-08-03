44a304d6adda494caa64dd88c829684b0fa3860e
Adithya Reddy <adithyakreddy6@gmail.com>
2017-08-03 21:58:42 +0530

    refactored and reorederd functions

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const fs = require('fs');
 const url = require('url');
 const path = require('path');
 
+function parseArguments() {
+  let comicNumber = '';
+  let downloadDirectory = __dirname;
+
+  if (process.argv[2] && !isNaN(process.argv[2])) {
+    comicNumber = process.argv[2];
+  }
+
+  if (comicNumber && process.argv[3]) {
+    downloadDirectory = process.argv[3];
+  } else if (!comicNumber && process.argv[2]) {
+    downloadDirectory = process.argv[2];
+  }
+
+  return { comicNumber, downloadDirectory };
+}
+
+function buildURL(comicNumber) {
+  return new url.URL(`https://xkcd.com/${comicNumber + '/'}info.0.json`);
+}
+
 function getImageFileDetails(xkcdJsonResponse) {
   const imageUrl = xkcdJsonResponse.img;
   const imageFileName = path.basename(url.parse(imageUrl).pathname);
@@ -17,22 +38,14 @@ function downloadImage(imageUrl, filePath) {
   http.get(imageUrl, response => response.pipe(file));
 }
 
-let comicNumber = '';
-if (process.argv[2]) {
-  comicNumber = process.argv[2];
-}
-
-const xkcdJsonUrl = new url.URL(
-  `https://xkcd.com/${comicNumber + '/'}info.0.json`
-);
-
+const { comicNumber, downloadDirectory } = parseArguments();
+const xkcdJsonUrl = buildURL(comicNumber);
 http.get(xkcdJsonUrl, response => {
   let body = '';
   response.on('data', d => (body += d));
   response.on('end', () => {
-    const imageFileDetails = getImageFileDetails(JSON.parse(body));
-    const imageUrl = imageFileDetails.imageUrl;
-    const filePath = path.join(__dirname, imageFileDetails.imageFileName);
+    const { imageUrl, imageFileName } = getImageFileDetails(JSON.parse(body));
+    const filePath = path.join(downloadDirectory, imageFileName);
     downloadImage(imageUrl, filePath);
   });
 });
